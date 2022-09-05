ddb953f65bd6c4daaf6b56c2d0d1b1a5e40951bb
Yuxiang Zong <104466890+YXZong0@users.noreply.github.com>
2022-09-05 17:24:21 +0200

    Update likelihood_qua.m

--- likelihood_qua.m
+++ likelihood_qua.m
@@ -3,19 +3,17 @@ function f = likelihood_qua(coefsig,nB,Y,A,B)
 % coefsig: initial parameters 
 % nB: the order of Bernstein
 % Y: observations
-% A,B bound
-    %h = @(t,y,sig_mul,sig_add) quadgk(@(z) arrayfun(@(t) htoInt(z,t,y,sig_mul,sig_add),t,'UniformOutput',false), 0, 9999);
+% A,B: boundary
     %fy = @(y,theta,sig_mul,sig_add,nB,A,B) integral2(@(z,t) htoInt(z,t,y,sig_mul,sig_add).*densityBernstein(t,nB,theta)./A,0,Inf,B,A+B);
     %fy = @(y,theta,sig_mul,sig_add,nB,A,B) quadgk(@(t) arrayfun(@(t) quadgk(@(z) htoInt(z,t,y,sig_mul,sig_add).*densityBernstein(t,nB,theta)./A,0,Inf),t),B,A+B);
-    fy = @(y,theta,sig_mul,sig_add,nB,A,B) quadgk(@(z) arrayfun(@(z) quadgk(@(t) htoInt(z,t,y,sig_mul,sig_add).*densityBernstein((t-B)/A,nB,theta)./A,B,A+B),z),0,Inf);
     %fy = @(y,theta,sig_mul,sig_add,nB,A,B) integral(@(z) arrayfun(@(z) integral(@(t) htoInt(z,t,y,sig_mul,sig_add).*densityBernstein((t-B)/A,nB,theta)./A,B,A+B),z),0,Inf);
-    theta = coefsig(1:(nB+1));
-    sig_mul = coefsig(nB+2);
-    sig_add = coefsig(nB+3);
-    fy_res = arrayfun(@(Y) fy(Y,theta,sig_mul,sig_add,nB,A,B), Y);
-    f = -sum(log(fy_res));
+    fy = @(y,theta,sig_mul,sig_add,nB,A,B) quadgk(@(z) arrayfun(@(z) quadgk(@(t) htoInt(z,t,y,sig_mul,sig_add).*densityBernstein((t-B)/A,nB,theta)./A,B,A+B),z),0,Inf);
+    theta = coefsig(1:(nB+1)); % coefficients 
+    sig_mul = coefsig(nB+2); % multiplicative std
+    sig_add = coefsig(nB+3); % additive std
+    fy_res = arrayfun(@(Y) fy(Y,theta,sig_mul,sig_add,nB,A,B),Y);
+    f = -sum(log(fy_res)); % to be minimized
     if f == Inf
         f = 99999; 
     end
-    %f=fy_res;
 end
