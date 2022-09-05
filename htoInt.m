a2cacaf1dc4cb19772f732ace98724a0718ab0c9
Yuxiang Zong <104466890+YXZong0@users.noreply.github.com>
2022-09-05 17:39:24 +0200

    h function in the likelihood function

--- htoInt.m
+++ htoInt.m
@@ -1,5 +1,5 @@
 function f = htoInt(z,t,y,sig_mul,sig_add)
-    % h function
+    % h function in the likelihood function
     %f = (lognpdf(z,t,sig_mul).*normpdf(y,z,sig_add));
     f = normpdf((log(z)-t)./sig_mul).*(normpdf((y-z)./sig_add))./(z*sig_add*sig_mul);
 end
