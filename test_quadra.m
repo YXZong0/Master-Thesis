47fdf57fdd9491202e29cd73b57fc092649ec53d
Yuxiang Zong <104466890+YXZong0@users.noreply.github.com>
2022-09-05 18:56:12 +0200

    test using toy sample

--- test_quadra.m
+++ test_quadra.m
@@ -8,4 +8,4 @@ n = 300; % sample size
 W = (a*betarnd(3,4,n,1)+b).*lognrnd(0,sig_mul,n,1)+normrnd(0,sig_add,n,1); %toy sample
 nB = 5; % order
 [res1,bic1] = Estimation_unknown(W,nB,a,b); % assume two std unknown 
-[res2,bic2] = Estimation_known(W,nB,a,b,sig1,sig2); % assume two std known
+[res2,bic2] = Estimation_known(W,nB,a,b,sig_mul,sig_add); % assume two std known
